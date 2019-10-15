72a569549aecae92b42b3c8c551c76934b2df3af
Julia Kharlamova <julia88.2010@mail.ru>
2019-10-15 20:18:18 +0300

    fetch home posts

--- src/components/Home/content.vue
+++ src/components/Home/content.vue
@@ -2,17 +2,17 @@
   <div class="container">
     <div class="home_container">
       <md-card v-for="(post, index) in posts" :key="index">
-          <md-card-media md-ratio="16:9">
-            <img :src="require(`../../assets/images/featured/${post.img}`)" alt="" />
+          <md-card-media md-ratio="4:3">
+            <img :src="post.img" alt="" />
           </md-card-media>
         <md-card-header>
-          <h2 class="title">{{post.title}}</h2>
+          <h2 class="md-title">{{post.title}}</h2>
           <div class="md-subhead">
-            {{post.description}}
+            {{post.desc}}
           </div>
         </md-card-header>
         <md-card-actions>
-          <Btn type="link" linkTo="#" :addClass="['small_link']">See review</Btn>
+          <Btn type="link" :linkTo="`posts/${post.id}`" :addClass="['small_link']">See review</Btn>
         </md-card-actions>
       </md-card>
     </div>
@@ -20,17 +20,26 @@
   </div>
 </template>
 <script>
- import posts from '../../assets/posts.js';
+//  import posts from '../../assets/posts.js';
 import Btn  from '../UI/Buutton.vue';
 export default {
  data () {
    return {
-     posts,
-     
+    
    }
  },
  components: {
    Btn
- }
+ },
+ computed: {
+   posts () {
+     return this.$store.getters['posts/getPosts'];
+   }
+ },
+ created() {
+   this.$store.dispatch('posts/getAllPosts', {
+     limit: 3
+   });
+ },
 }
 </script>
